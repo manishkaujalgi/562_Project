02b4af56e5936c69acbc8ad71bc63c76dafbab02
manishkaujalgi <mmukundkaujalgi@hawk.iit.edu>
2018-12-02 17:24:26 -0600

    Displaying Items on Homepage from the database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var Item = require('../models/item');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Item.find(function(err, result){
+
+    var limit = 3;
+    var groupOfItems = [];
+    for(var a=0; a < result.length; a = a + limit ){
+        groupOfItems.push(result.slice(a, a + limit));
+
+    }
+
+    res.render('index', { title: 'Express', items: groupOfItems });
+
+  });
+  
 });
 
 module.exports = router;
